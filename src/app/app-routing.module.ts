19aa0414316fafdcadd6c3ac062a0f1cb15d6d1a
riquemath <fredabisai@gmail.com>
2020-09-22 11:01:28 +0300

    Add routes in routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContainersComponent } from './containers/containers.component';
 
-
 const routes: Routes = [
   {
     path: '',
     loadChildren: () =>
-      import('./containers/containers.module').then((m) => m.ContainersModule)
-  }
+      import('./containers/containers.module').then((m) => m.ContainersModule),
+  },
+  {
+    path: '**',
+    loadChildren: () =>
+      import('./containers/containers.module').then((m) => m.ContainersModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
