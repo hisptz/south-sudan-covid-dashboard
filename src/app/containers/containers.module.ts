7660c4ea07dcb4610439beb940d6240274ab8e10
riquemath <fredabisai@gmail.com>
2020-09-23 15:52:04 +0300

    Change the structure of components

--- src/app/containers/containers.module.ts
+++ src/app/containers/containers.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainersComponent } from './containers.component';
-import { CaseSurveillanceTestingComponent } from './case-surveillance-testing/case-surveillance-testing.component';
+import { CaseSurveillanceTestingComponent } from './surveillance/case-surveillance-testing/case-surveillance-testing.component';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
-import { MapSectionComponent } from './map-section/map-section.component';
-import { ConfirmedCasesAndDeathsSectionComponent } from './confirmed-cases-and-deaths-section/confirmed-cases-and-deaths-section.component';
-import { TestsConductedAndPositiveCasesComponent } from './tests-conducted-and-positive-cases/tests-conducted-and-positive-cases.component';
-import { SummaryTableComponent } from './summary-table/summary-table.component';
+import { MapSectionComponent } from './surveillance/map-section/map-section.component';
+import { ConfirmedCasesAndDeathsSectionComponent } from './surveillance/confirmed-cases-and-deaths-section/confirmed-cases-and-deaths-section.component';
+import { TestsConductedAndPositiveCasesComponent } from './surveillance/tests-conducted-and-positive-cases/tests-conducted-and-positive-cases.component';
+import { SummaryTableComponent } from './surveillance/summary-table/summary-table.component';
 const routes: Routes = [
   {
     path: '',
