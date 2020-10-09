8a1c4e1bbe557b99710afc94240d41283a791376
riquemath <fredabisai@gmail.com>
2020-10-09 16:34:00 +0300

    Change names of states

--- src/app/shared/components/map-visualization/map-visualization.component.ts
+++ src/app/shared/components/map-visualization/map-visualization.component.ts
@@ -20,77 +20,122 @@ export class MapVisualizationComponent implements OnInit, AfterViewInit {
   currentChart: any;
   currentDevice: string;
   constructor() {
-    this.mapGeo = MAP_GEO.ss;
     this.mappingOrgUnits = MAP_GEO.ssOrgUnits;
+    this.mapGeo = this.getCustomMapData();
   }
 
   ngOnInit(): void {}
   ngAfterViewInit() {
-    if (this.analyticsData) {
+    if (this.analyticsData && this.mapGeo) {
       this.drawMap();
     }
   }
 
   drawMap() {
-    const container = document.createElement('div');
-    const chartHolder = document.getElementById('mapVisualization');
-    chartHolder.appendChild(container);
-
-    this.currentChart = Highcharts.mapChart({
-      chart: {
-        map: this.mapGeo,
-        renderTo: container,
-      },
-      title: {
-        text: this.mapTitle,
-      },
-      subtitle: {
-        text: 'South Sudan',
-      },
-      mapNavigation: {
-        enabled: true,
-        buttonOptions: {
-          verticalAlign: 'top',
+    if (this.mapGeo) {
+      const container = document.createElement('div');
+      const chartHolder = document.getElementById('mapVisualization');
+      chartHolder.appendChild(container);
+      this.currentChart = Highcharts.mapChart({
+        chart: {
+          map: this.mapGeo,
+          renderTo: container,
         },
-      },
-      colorAxis: {
-        min: 0,
-        max: 20,
-        minColor: '#F6F7F7',
-        maxColor: '#DE001E',
-      },
-      credits: {
-        enabled: false,
-      },
-      series: [
-        {
-          type: undefined,
-          data: this.getRandomData(),
-          name: 'Confirmed cases',
-          states: {
-            hover: {
-              color: '#FFFFFF',
+        title: {
+          text: this.mapTitle,
+        },
+        subtitle: {
+          text: 'South Sudan',
+        },
+        mapNavigation: {
+          enabled: true,
+          buttonOptions: {
+            verticalAlign: 'top',
+          },
+        },
+        colorAxis: {
+          min: 0,
+          max: 20,
+          minColor: '#F6F7F7',
+          maxColor: '#DE001E',
+        },
+        credits: {
+          enabled: false,
+        },
+        series: [
+          {
+            type: undefined,
+            data: this.getRandomData(),
+            name: 'Confirmed cases',
+            states: {
+              hover: {
+                color: '#FFFFFF',
+              },
+            },
+            dataLabels: {
+              enabled: true,
+              format: '{point.name} ({point.value})',
             },
           },
-          dataLabels: {
-            enabled: true,
-            format: '{point.name} ({point.value})',
-          },
-        },
-      ],
-    });
+        ],
+      });
+    }
   }
+  getCustomMapData() {
+    let mapData = MAP_GEO.ss;
+    const features = [];
+    if (mapData && mapData.features && mapData.features.length) {
+      for (const feature of mapData.features) {
+        const key = feature.properties['hc-key'];
+        const name = this.getLocationValidName(key);
+        const properties =
+          feature && feature.properties
+            ? { ...feature.properties, 'woe-name': name, name }
+            : features;
+        const newFeature = { ...feature, properties };
+        features.push(newFeature);
+      }
+     // console.log({ geo: MAP_GEO.ss });
+      mapData = { ...mapData, features };
+    }
 
+    return mapData;
+  }
   getRandomData() {
     const mapData = [];
-    for (const feature of this.mapGeo.features) {
-      const key = feature.properties['hc-key'];
-      const value = this.getAnalyticsValue(key);
-      mapData.push([key, value]);
+    if (this.mapGeo && this.mapGeo.features && this.mapGeo.features.length) {
+      for (const feature of this.mapGeo.features) {
+        const key =
+          feature && feature.properties && feature.properties['hc-key']
+            ? feature.properties['hc-key']
+            : '';
+        const value = this.getAnalyticsValue(key);
+        mapData.push([key, value]);
+      }
     }
     return mapData;
   }
   getAnalyticsValue(key) {
+    const orgUnitId = this.getOrgUnitIdByHCKey(key);
+    const analyticsObj = orgUnitId
+      ? _.find(this.analyticsData || [], (item) => item.orgUnit === orgUnitId)
+      : null;
+    const value = analyticsObj && analyticsObj.value ? analyticsObj.value : 0;
+    return value ? value : 0;
+  }
+  getLocationValidName(key): string {
+    const mappingObj = this.getMappingObjectForMapConfig(key);
+    const orgUnitName =
+      mappingObj && mappingObj.displayName ? mappingObj.displayName : null;
+    return orgUnitName;
+  }
+  getOrgUnitIdByHCKey(key) {
+    const mappingObj = this.getMappingObjectForMapConfig(key);
+    const orgUnitId =
+      mappingObj && mappingObj.orgUnitId ? mappingObj.orgUnitId : null;
+    return orgUnitId;
+  }
+  getMappingObjectForMapConfig(key): any {
     const mapConfig =
       this.mappingOrgUnits && this.mappingOrgUnits.mapConfig
         ? this.mappingOrgUnits.mapConfig
@@ -99,12 +144,6 @@ export class MapVisualizationComponent implements OnInit, AfterViewInit {
       mapConfig || [],
       (config) => config['hc-key'] === key
     );
-    const orgUnitId =
-      mappingObj && mappingObj.orgUnitId ? mappingObj.orgUnitId : null;
-    const analyticsObj = orgUnitId
-      ? _.find(this.analyticsData || [], (item) => item.orgUnit === orgUnitId)
-      : null;
-    const value = analyticsObj && analyticsObj.value ? analyticsObj.value : 0;
-    return value ? value : 0;
+    return mappingObj;
   }
 }
